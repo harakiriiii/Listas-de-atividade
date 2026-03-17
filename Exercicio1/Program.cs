1f27216f7c79a439e7770792764ac75ff7aab162
poltergeist <dropecaslu333@gmail.com>
2026-03-17 16:31:40 -0300

    Exercicio1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("-------------------");
+Console.WriteLine("Altura do Retângulo");
+Console.WriteLine("--------------------");
+
+decimal Altura = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine("-------------------");
+Console.WriteLine("Largura do Retângulo");
+Console.WriteLine("--------------------");
+
+decimal Largura = Convert.ToDecimal(Console.ReadLine());
+
+decimal Volume = Largura * Altura;
+Console.WriteLine($"O volume do retangulo é:{Volume}");
+
