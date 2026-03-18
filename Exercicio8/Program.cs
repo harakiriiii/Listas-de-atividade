ff5d29fb4515b63948355f010696dc0222e11c19
poltergeist <dropecaslu333@gmail.com>
2026-03-18 13:51:14 -0300

    Exercicio8

---
+++ Exercicio8/Program.cs
@@ -0,0 +1,13 @@
+﻿﻿//8. Crie um programa para verificar se um número é primo.
+
+System.Console.Write("Digite um numero para verificar se e par ou impar: ");
+decimal num = Convert.ToDecimal(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    System.Console.WriteLine("O numero e PAR");
+}
+else
+{
+    System.Console.WriteLine("O numero e impar");
+}
