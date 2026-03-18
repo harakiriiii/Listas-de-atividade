6883107ed4606b2ff767de6e38b023998357f1a1
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:08:09 -0300

    Exercicio21

---
+++ Exercicio21/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿// 20. Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada de N. Mostre a
+// tabuada na forma:
+
+System.Console.Write("Digite um valor para verificar a fatorial: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int soma = 1;
+
+for(int i = 1; i<= n; i++)
+{
+    soma *= i;
+}
+System.Console.WriteLine(soma);
