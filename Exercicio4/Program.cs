f57319df669a92710e4dd6ad2d77241121c22c9b
poltergeist <dropecaslu333@gmail.com>
2026-03-18 13:48:38 -0300

    Exercicio4

---
+++ Exercicio4/Program.cs
@@ -0,0 +1,8 @@
+﻿﻿//4. Crie um programa para converter a temperatura da escala Celsius para a escala Fahrenheit
+
+System.Console.Write("Digite a temperatura em Celsius: ");
+decimal celsius = Convert.ToDecimal(Console.ReadLine());
+
+decimal converter = ( celsius * 9/5) + 32;
+
+System.Console.WriteLine("Conversao para Fahrenheit: " + converter);
