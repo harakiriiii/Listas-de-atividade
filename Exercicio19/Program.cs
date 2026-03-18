40b7493f4414c9a309dd4028993b83fee018e32e
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:07:06 -0300

    Exercicio19

---
+++ Exercicio19/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿// //19 Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de três e que se
+// encontram no conjunto dos números de 1 até 500.
+
+for(int i = 1; i <= 500; i++)
+{
+    if(i%2 != 0 && i%3 == 0)
+    {
+        System.Console.WriteLine(i);
+    }
+}
