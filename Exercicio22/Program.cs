d7dc87e4640ed4f74443edd80eb3c924fe039c94
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:08:44 -0300

    Exercicio22

---
+++ Exercicio22/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿// 22. Escreva um programa que leia um número e imprima a sequência de Fibonacci até esse número.
+
+System.Console.WriteLine("Digite o primeiro numero: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Digite o primeiro numero: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0; i < 10; i++)
+{
+    int proximo = a + b;
+    Console.WriteLine(proximo);
+
+    a = b;
+    b = proximo;
+}
