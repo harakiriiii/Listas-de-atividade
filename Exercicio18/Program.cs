13e7ff2735725db43ad4754a9506225bf558f72f
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:06:28 -0300

    Exercicio18

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,9 @@
+﻿﻿// 18. Escrever um algoritmo que gera e escreve os números ímpares entre 100 e 200.
+
+for(int i = 100; i <= 200; i++)
+{
+    if(i%2 != 0)
+    {
+        System.Console.WriteLine(i);
+    }
+}
