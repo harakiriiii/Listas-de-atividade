2932721b8f879884ede05dc60b6e549f80cea783
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:01:26 -0300

    Exercicio12

---
+++ Exercicio12/Program.cs
@@ -0,0 +1,14 @@
+﻿﻿// 12. Faça um algoritmo para ler o salário de um funcionário e aumentá-lo em 15%. Após o aumento, desconte 8% de
+// impostos. Imprima o salário inicial, o salário com o aumento e o salário final.
+
+System.Console.Write("Digite o seu salario: ");
+decimal salario = Convert.ToDecimal(Console.ReadLine());
+
+decimal aumento = salario + (salario * 0.15m);
+
+System.Console.WriteLine("Seu salario com o aumento e: " + aumento);
+
+decimal imposto = aumento - (aumento * 0.08m);
+
+System.Console.WriteLine("Com os impostos seu salario e: " + imposto);
+System.Console.WriteLine("faz o L");
