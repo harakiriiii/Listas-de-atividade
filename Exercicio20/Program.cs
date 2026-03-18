0bca89e0f9c60276aa49da971c3f74afeb094f08
poltergeist <dropecaslu333@gmail.com>
2026-03-18 14:07:34 -0300

    Exercicio20

---
+++ Exercicio20/Program.cs
@@ -0,0 +1,10 @@
+﻿﻿// 20. Escrever um algoritmo que leia um valor para uma variável N de 1 a 10 e calcule a tabuada de N. Mostre a
+// tabuada na forma:
+
+System.Console.Write("Digite um numero para ver a tabuada: ");
+decimal n = Convert.ToDecimal(Console.ReadLine());
+
+for(int i = 0; i <=10; i++)
+{
+    System.Console.WriteLine($"{n} X {i} = {n*i}");
+}
