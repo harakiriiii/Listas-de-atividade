94d032061dcff1d2c0ce75dcd3b8d0139d725a43
poltergeist <dropecaslu333@gmail.com>
2026-03-18 13:50:02 -0300

    Exercicio6

---
+++ Exercicio6/Program.cs
@@ -0,0 +1,19 @@
+﻿﻿// Crie um programa para calcular a média harmônica das notas de um aluno
+
+decimal[] notas = new decimal[4];
+
+System.Console.Write("Digite a primeira nota do aluno: ");
+notas[0] = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Digite a segunda nota do aluno: ");
+notas[1] = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Digite a terceira nota do aluno: ");
+notas[2] = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Digite a quarta nota do aluno: ");
+notas[3] = Convert.ToDecimal(Console.ReadLine());
+
+decimal mediaHarmonica = 4 / (1 / notas[0] + 1 / notas[1] + 1 / notas[2] + 1 / notas[3]);
+
+System.Console.WriteLine("A media Harmonica e: " + mediaHarmonica);
