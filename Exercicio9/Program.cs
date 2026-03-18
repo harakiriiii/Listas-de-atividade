f1e081e337a8aa6c337f3337fb884b509fff8949
poltergeist <dropecaslu333@gmail.com>
2026-03-18 13:51:46 -0300

    Exercicio9

---
+++ Exercicio9/Program.cs
@@ -0,0 +1,12 @@
+﻿﻿// 9. A imobiliária Imóbilis vende apenas terrenos retangulares. Faça um algoritmo para ler as dimensões de um
+// terreno e depois exibir a área do terreno.
+
+System.Console.Write("Digite a largura do terreno: ");
+decimal comprimento = Convert.ToDecimal(Console.ReadLine());
+
+System.Console.Write("Digite a largura do altura: ");
+decimal altura = Convert.ToDecimal(Console.ReadLine());
+
+decimal area = altura * comprimento;
+
+System.Console.WriteLine("A area do Imovel: " + area);
